98a98a28f5fa768e67ecc6e72aee6d3b4e7d92ae
homework <iamwangcan@163.com>
2015-11-30 07:24:22 +0800

    add the yii aotu

---
+++ module/Codelib/src/Codelib/Controller/YiiController.php
@@ -0,0 +1,99 @@
+<?php
+namespace Codelib\Controller;
+
+use \Light\Mvc\Controller\ControllerAbstract as ControllerAbstract;
+
+class YiiController extends ControllerAbstract
+{
+    public function __construct()
+    {
+        $this->currentModule = 'Codelib';
+        parent::__construct();
+
+        $this->link = $this->_connectDb();
+        //echo 'init ciauto';
+    }
+
+    public function index()
+    {
+        $templates['controller'] = file_get_contents($this->modulePath . '/data/controller.txt');
+        $templates['model'] = file_get_contents($this->modulePath . '/data/model.txt');
+        $templates['model_search'] = file_get_contents($this->modulePath . '/data/model_search.txt');
+        $templates['view'] = $this->modulePath . '/data/views';
+        
+        $databases = array('workbench_website', 'workbench_paytrade', 'workbench_passport');
+        foreach ($databases as $database) {
+            $tables = $this->getTables($database);
+            if (is_array($tables) && !empty($tables)) {
+                foreach ($tables as $table => $comment) {
+                    $this->createFiles($database, $table, $templates);
+                }
+            }
+        }
+    }
+
+    protected function getTables($database)
+    {
+        $tables = array();
+        $sql = "SELECT * FROM `information_schema`.`tables` WHERE `TABLE_SCHEMA` = '{$database}'";
+        $result = mysql_query($sql);
+        while ($row = mysql_fetch_array($result)) {
+            $tables[$row['TABLE_NAME']] = $row['TABLE_COMMENT'];
+        }
+        //print_r($tables);exit();
+        return $tables;
+    }
+    
+    protected function createFiles($database, $table, $templates)
+    {
+        $tableBase = substr($table, strpos($table, '_') + 1);
+		$tableFirst = str_replace(' ', '', ucwords(str_replace('_', ' ', $tableBase)));
+		$databaseBase = str_replace('workbench_', '', $database);
+
+		$views = $templates['view'];
+		$viewsTarget = dirname($views) . '/yii2/' . $databaseBase . '/views/' . str_replace('_', '-', $tableBase);
+		if (!is_dir($viewsTarget)) {
+		    mkdir($viewsTarget, 0777, true);
+		}
+		foreach (array('_form.php', 'add.php', 'listinfo.php', 'update.php', 'view.php') as $viewFile) {
+			copy($views . '/' . $viewFile, $viewsTarget . '/' . $viewFile);
+		}
+		unset($templates['view']);
+
+        foreach ($templates as $template => $contentBase) {
+            
+            $fieldInfos = $this->getFieldInfos($database, $table);
+            $targetStrs = array("\r", '#FIELDINFOS#', '#MODULE#', '#CLASS#', '#TABLE#');
+            $replaceStrs = array('', $fieldInfos, $databaseBase, $tableFirst, $tableBase);
+            $content = str_replace($targetStrs, $replaceStrs, $contentBase);
+        
+            $filePath = $this->modulePath . '/data/yii2/' . $databaseBase . '/' . str_replace('_', '/', $template);
+            if (!is_dir($filePath)) {
+                mkdir($filePath, 0777, true);
+            }
+            $fileName = $filePath . '/' . $tableFirst . 'Controller.php';
+            $fileName = $template == 'controller' ? $fileName : str_replace('Controller', '', $fileName);
+            
+            echo $fileName . '<br />';
+        
+            $strlen = file_put_contents($fileName, $content);
+        }
+    }
+    
+    protected function getFieldInfos($database, $table)
+    {
+        $sql = "SELECT * FROM `information_schema`.`columns` WHERE `TABLE_SCHEMA` = '{$database}' AND `TABLE_NAME` = '{$table}'";
+        $result = mysql_query($sql);
+        $fieldInfos = array();
+        $string = '';
+        while ($row = mysql_fetch_array($result)) {
+            $field = $row['COLUMN_NAME'];
+            $comment = empty($row['COLUMN_COMMENT']) ? $field : $row['COLUMN_COMMENT'];
+            //$string .=  "            '{$field}' => array('name' => '{$comment}'),\n";
+            $string .=  "            '{$field}' => '{$comment}',\n";
+            $fieldInfos['fields'][$row['COLUMN_NAME']] = $row['COLUMN_COMMENT'];
+        }
+    
+        return $string . '        ';
+    }
+}
